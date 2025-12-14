7752bfa694afb99fe9f4d4b364fa4e7a306d0480
Joshua Stretton <89641074+Stretz@users.noreply.github.com>
2025-12-14 01:50:57 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,20 @@
+   -- Buff System For FiveM
+   --  Copyright (C) 2025  zStretz
+
+   --  This program is free software: you can redistribute it and/or modify
+   --  it under the terms of the GNU General Public License as published by
+   --  the Free Software Foundation, either version 3 of the License, or
+   --  (at your option) any later version.
+
+   --  This program is distributed in the hope that it will be useful,
+   --  but WITHOUT ANY WARRANTY; without even the implied warranty of
+   --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   --  GNU General Public License for more details.
+
+   --  You should have received a copy of the GNU General Public License
+   --  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 local Bridge = exports['community_bridge']:Bridge()
 local activeBuffs = {}
 local effectsRunning = false
@@ -197,3 +214,4 @@ AddEventHandler('playerDropped', function ()
 
     DebugPrint("All buffs cleared on logout.")
 end)
+
